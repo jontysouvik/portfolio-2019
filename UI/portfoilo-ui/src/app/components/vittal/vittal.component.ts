726388931e362f3197e3d476b073aa60e9c66ccc
Nellutla <Sundara.Nellutla@thomsonreuters.com>
2019-10-09 14:40:57 +0530

    vittalNewUpdate

--- UI/portfoilo-ui/src/app/components/vittal/vittal.component.ts
+++ UI/portfoilo-ui/src/app/components/vittal/vittal.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class VittalComponent implements OnInit {
 
   constructor() { }
-
+  name = "Vittal Nellutla";
+  image ="assets/vittal.jpg";
   ngOnInit() {
   }
 
