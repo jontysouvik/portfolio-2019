22ebb5594a6c631ac79a9d02fecd612ba79ad0ce
Nellutla <Sundara.Nellutla@thomsonreuters.com>
2019-10-09 14:44:38 +0530

    vittalbindingsadded

--- UI/portfoilo-ui/src/app/components/vittal/vittal.component.ts
+++ UI/portfoilo-ui/src/app/components/vittal/vittal.component.ts
@@ -10,6 +10,9 @@ export class VittalComponent implements OnInit {
   constructor() { }
   name = "Vittal Nellutla";
   image ="assets/vittal.jpg";
+  vr4utut = "https://www.youtube.com/channel/UCOMkA8ZCLGhDDVapX1hAXzA";
+  vr4uent = "https://www.youtube.com/channel/UCKL_sSDEN5Dahf_8851Z18g";
+  vitblog = "https://sundarasaivittal.blogspot.com/";
   ngOnInit() {
   }
 
